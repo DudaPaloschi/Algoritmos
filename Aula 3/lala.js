998be210f3b9dd48e63dc1e38bcfc8db4416ed71
DudaPaloschi <162522654+DudaPaloschi@users.noreply.github.com>
2024-03-08 21:59:23 -0300

    Add files via upload

---
+++ Aula 3/lala.js
@@ -0,0 +1,4 @@
+const serie = "Stranger Things"
+const temporadas = 4
+console.log(typeof serie) //string
+console.log(typeof temporadas) //number
---
+++ Aula 3/lalala.js
@@ -0,0 +1,22 @@
+//Este é um comentário de uma linha única
+
+/*Este é um comentário 
+em várias
+linhas*/
+
+//Este comando imprime algo no terminal
+
+console.log("Olá mundo!")
+
+//Comando com variaveis
+
+//Imprimindo mais de uma linha no console separando por vírgulas
+
+const nome = "Eduarda"
+const sobrenome = "Martins"
+const idade = "22"
+const souEstudante = true
+
+console.log("Meu nome é" , nome , sobrenome , ", tenho", idade , ",Sou estudante?", souEstudante )
+
+//Meu nome é Eduarda Martins, tenho 22 anos, sou estudante
